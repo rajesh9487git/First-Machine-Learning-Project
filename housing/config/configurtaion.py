2c77705a5baa88cbf5d8a2b11c61815be2bf6976
rajesh9487@gmail.com <rajesh9487@gmail.com>
2022-08-27 21:34:51 +0530

    commiting

--- housing/config/configurtaion.py
+++ housing/config/configurtaion.py
@@ -1,10 +1,27 @@
 from housing.entity.config_entity import DataIngestionConfig, DataTransformationConfig,DataValidationConfig, /
 ModelTrainerConfig,ModelEvaluationConfig,ModelPusherConfig,TrainingPipelineConfig
 
+from housing.util.util import read_yaml_file
+from housing.exception import HousingException
+from housing.logger import logging
+from housing.constant import *
+import os,sys
+from housing.entity.config_entity import *
+
+
+
 class Configuration:
 
-    def __init__(self)-> None:
-        pass
+    def __init__(self, 
+      config_file_path:str= CONFIG_FILE_PATH,
+      current_time_stamp:str= CURRENT_TIME_STAMP)-> None:
+      try:
+        self.conif_info= read_yaml_file(file_path=config_file_path)
+        self.training_pipeline_config= self.get_training_pipeline_config()
+        self.time_stamp=current_time_stamp
+      except Exception as e:
+        raise HousingException(e, sys)  from e 
+        
 
     def get_data_ingestion_config(self):
         pass
@@ -24,7 +41,19 @@ class Configuration:
     def get_model_pusher_config(self):
         pass
 
-    def get_training_pipeline_config(self):
-        pass
+    def get_training_pipeline_config(self)->TrainingPipelineConfig:
+        try:
+            training_pipeline_config = self.conif_info[TRAINING_PIPELINE_CONFIG_KEY]
+            artifact_dir= os.path.join(ROOT_DIR,
+             training_pipeline_config[TRAINING_PIPELINE_NAME_KEY], 
+            training_pipeline_config[TRAINING_PIPELINE_ARTIFACT_DIR_KEY]
+            )
+
+            training_pipeline_config= TrainingPipelineConfig(artifact_dir=artifact_dir)
+            logging.info(f"Training pipeline config:{training_pipeline_config}")
+            return training_pipeline_config
+
+        except Exception as e:
+            raise HousingException(e, sys) from e  
     
 
