9158c661fe5bec0dc728a2e6bb46eeb7857e538c
rajesh9487@gmail.com <rajesh9487@gmail.com>
2022-09-07 12:30:48 +0530

    updating

---
+++ demo.py
@@ -0,0 +1,19 @@
+from housing.pipeline.pipeline import Pipeline
+from housing.exception import HousingException
+from housing.logger import logging
+from housing.config.configuration import Configuration
+import os,sys
+
+def main():
+    try:
+        config_path = os.path.join("config","config.yaml")
+        pipeline= Pipeline(Configuration(config_file_path=config_path))
+        pipeline.run_pipeline()
+    except Exception as e:
+        logging.error(f"{e}")
+        print(e)
+
+
+if __name__=="__main__":
+    main()        
+
