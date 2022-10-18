c78626a3896e21e9332bce7d4219781c5099a1f8
rajesh9487@gmail.com <rajesh9487@gmail.com>
2022-10-18 20:58:56 +0530

    updated

--- housing/entity/model_factory.py
+++ housing/entity/model_factory.py
@@ -245,6 +245,131 @@ class ModelFactory:
         except Exception as e:
             raise HousingException(e, sys) from e
 
-            
-                 
-                                           
+
+    def get_initialized_model_list(self)-> List[InitializedModelDetail]:
+        """
+        This function will return a list of model details.
+        return List[ModelDetail]
+        """
+
+        try:
+
+            initialized_model_list=[]
+            for model_serial_number in self.models_initialization_config.keys():
+
+                models_initialization_config= self.models_initialization_config[model_serial_number]
+                model_obj_ref= ModelFactory.class_for_name(module_name= model_initialization_config[MODULE_KEY], 
+                                                            class_name= models_initialization_config[CLASS_KEY])
+
+                model = model_obj_ref()
+
+                if PARAM_KEY in models_initialization_config:
+                    model_obj_property_data= dict(models_initialization_config[PARAM_KEY])
+                    model= ModelFactory.update_property_of_class(instance_ref=model, property_data=model_obj_property_data)
+
+                param_grid_search= models_initialization_config[SEARCH_PARAM_GRID_KEY]
+                model_name= f"{models_initialization_config[MODULE_KEY]}.{models_initialization_config[CLASS_KEY]}"
+
+                model_initialization_config= InitializedModelDetail(model_serial_number=model_serial_number,
+                                                                      model=model,
+                                                                      param_grid_search=param_grid_search,
+                                                                      model_name=model_name)
+
+                initialized_model_list.append(model_initialization_config)     
+
+            self.initialized_model_list= initialized_model_list
+            return self.initialized_model_list
+
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    def initiate_best_parameter_search_for_initialized_model(self, initialized_model: InitializedModelDetail,
+                                                              input_feature,
+                                                              output_feature)-> GridSearchedBestModel:
+        """
+        initiate_best_model_parameter_search(): function will perform paramter search operation and
+        it will return you the best optimistic  model with best paramter:
+        estimator: Model object
+        param_grid: dictionary of paramter to perform search operation
+        input_feature: your all input features
+        output_feature: Target/Dependent features
+        ================================================================================
+        return: Function will return a GridSearchOperation
+        """
+        try:
+            return self.execute_grid_search_operation(initialized_model=initialized_model, 
+                                                      input_feature= input_feature, 
+                                                      output_feature= output_feature)
+
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    def initiate_best_parameter_search_for_initialized_models(self,
+                                                              initialized_model_list:List[InitializedModelDetail],
+                                                              input_feature,
+                                                              output_feature)-> List[GridSearchedBestModel]:
+
+        try:
+            self.grid_searched_best_model_list=[]
+            for initialized_model_list in initialized_model_list:
+                grid_searched_best_model= self.initiate_best_parameter_search_for_initialized_model(
+                                                                                                    initialized_model=initialized_model_list, 
+                                                                                                    input_feature=input_feature, 
+                                                                                                    output_feature=output_feature)
+                self.grid_searched_best_model_list.append(grid_searched_best_model)
+            return self.grid_searched_best_model_list
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    @staticmethod
+    def get_model_detail(model_details:List[InitiaizedModelDetail],
+                         model_serial_number:str)-> InitializedModelDetail:
+        """
+        This function return ModelDetail
+        """
+        try:
+            for model_data in model_details:
+                if model_data.model_serial_number==model_serial_number:
+                    return model_data
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+
+    @staticmethod
+    def get_best_model_from_grid_searched_best_model_list(grid_searched_best_model_list:List[GridSearchedBestModel],
+                                                          base_accuracy= 0.6)-> BestModel:
+
+        try:
+            best_model=None
+            for grid_searched_best_model in grid_searched_best_model_list:
+                if base_accuracy< grid_searched_best_model.best_score:
+                    logging.info(f"Acceptable model found:{grid_searched_best_model}")   
+                    base_accuracy= grid_searched_best_model.best_score
+
+                    best_model= grid_searched_best_model
+
+            if not best_model:
+                raise Exception(f"None of Model has base accuracy: {base_accuracy}")   
+            logging.info(f"Best model: {best_model}")
+            return best_model
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    def get_best_model(self, X, y, base_accuracy=0.6)-> BestModel:
+        try:
+
+            logging.info("Started Initializing model from config file")
+            initialized_model_list= self.get_initialized_model_list()
+            logging.info(f"Initialized model: {initialized_model_list}")   
+            grid_searched_best_model_list= self.initiate_best_parameter_search_for_initialized_models(
+                initialized_model_list=initialized_model_list, 
+                input_feature=X, 
+                output_feature=y)
+            return ModelFactory.get_best_model_from_grid_searched_best_model_list(grid_searched_best_model_list, 
+                                                                                  base_accuracy=base_accuracy)
+
+        except Exception as e:
+            raise HousingException(e, sys)from e
+                                                                                       
+
+
