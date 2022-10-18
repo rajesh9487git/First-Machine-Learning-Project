04c0c2014d48611a778959a54cfd505b95ac171c
rajesh9487@gmail.com <rajesh9487@gmail.com>
2022-10-18 12:00:20 +0530

    updatd

--- housing/entity/model_factory.py
+++ housing/entity/model_factory.py
@@ -149,5 +149,102 @@ def get_sample_model_config_yaml_file(export_dir:str):
     except Exception as e:
         raise HousingException(e, sys) from e
         
-                
 
+class ModelFactory:
+    def __init__(self, model_config_path:str=None):
+        try:
+            self.config:dict= ModelFactory.read_params(model_config_path) 
+
+            self.grid_search_cv_module:str= self.config[GRID_SEARCH_KEY][MODULE_KEY] 
+            self.grid_search_class_name:str= self.config[GRID_SEARCH_KEY][CLASS_KEY]   
+            self.grid_search_property_data:dict= dict(self.config[GRID_SEARCH_KEY][PARAM_KEY])  
+
+            self.models_initialization_config:dict= dict(self.config[MODEL_SELECTION_KEY]) 
+
+            self.initialized_model_list= None
+            self.grid_searched_best_model_list= None
+
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+
+    @staticmethod
+    def update_property_of_class(instance_ref:object, property_data:dict):
+        try:
+            if not isinstance(property_data, dict):
+                raise Exception("property_data parameter required to dictionary")
+            print(propert_data)   
+
+            for key, value in property_data.items():
+                logging.info(f"Executing:${str(instance_ref)}.{key}={value}")
+                setattr(instance_ref, key, value)
+            return instance_ref
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    @staticmethod
+    def read_params(config_path:str)-> dict:
+        try:
+            with open(config_path) as yaml_file:
+                config: dict= yaml.safe_load(yaml_file)
+            return config
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    @staticmethod
+    def class_for_name(module_name:str, class_name:str):
+        try:
+
+            # load the module, will raise ImportError if module cannot be loaded
+            module= importlib.import_module(module_name)
+            # get the class, will raise Attribute error if class cannot be found
+            logging.info(f"Executing command : from {module} import {class_name}")
+            class_ref= getattr(module, class_name)
+            return class_ref
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+    def execute_grid_search_operation(self, initialized_model: InitializedModelDetail, input_feature,
+                                      output_feature) -> GridSearchedBestModel:
+        """
+        excute_grid_search_operation(): function will perform paramter search operation and
+        it will return you the best optimistic  model with best paramter:
+        estimator: Model object
+        param_grid: dictionary of paramter to perform search operation
+        input_feature: your all input features
+        output_feature: Target/Dependent features
+        ================================================================================
+        return: Function will return GridSearchOperation object
+        """
+        try:
+            # instantiating GridSearchCV class
+            
+           
+            grid_search_cv_ref = ModelFactory.class_for_name(module_name=self.grid_search_cv_module,
+                                                             class_name=self.grid_search_class_name
+                                                             )
+
+            grid_search_cv = grid_search_cv_ref(estimator=initialized_model.model,
+                                                param_grid=initialized_model.param_grid_search)
+            grid_search_cv = ModelFactory.update_property_of_class(grid_search_cv,
+                                                                   self.grid_search_property_data)
+
+            
+            message = f'{">>"* 30} f"Training {type(initialized_model.model).__name__} Started." {"<<"*30}'
+            logging.info(message)
+            grid_search_cv.fit(input_feature, output_feature)
+            message = f'{">>"* 30} f"Training {type(initialized_model.model).__name__}" completed {"<<"*30}'
+            grid_searched_best_model = GridSearchedBestModel(model_serial_number=initialized_model.model_serial_number,
+                                                             model=initialized_model.model,
+                                                             best_model=grid_search_cv.best_estimator_,
+                                                             best_parameters=grid_search_cv.best_params_,
+                                                             best_score=grid_search_cv.best_score_
+                                                             )
+            
+            return grid_searched_best_model
+        except Exception as e:
+            raise HousingException(e, sys) from e
+
+            
+                 
+                                           
