b8b501eefe09c6c28995b9b638a9ab19e795cc21
rajesh9487@gmail.com <rajesh9487@gmail.com>
2022-11-16 12:32:12 +0530

    updated

--- demo.py
+++ demo.py
@@ -8,7 +8,10 @@ def main():
     try:
         config_path = os.path.join("config","config.yaml")
         pipeline= Pipeline(Configuration(config_file_path=config_path))
-        pipeline.run_pipeline()
+        # pipeline.run_pipeline()
+        pipeline.start()
+        logging.info("main function execution completed.")
+        
     except Exception as e:
         logging.error(f"{e}")
         print(e)
